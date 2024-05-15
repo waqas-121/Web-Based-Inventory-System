77c5508b556679477bed14ce82db1ceab45c6d72
Zain <syed.muhammad.zain.akbber@gmail.com>
2024-05-15 12:17:02 +0500

    Add project files.

---
+++ Web Inventory/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web_Inventory.Models;
+
+namespace Web_Inventory.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+
+		}
+		public DbSet<Inventory> inventories { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Inventory>().HasData(
+				new Inventory { Id = 1, ProductName = "Ball Pen", Quantity = 150, Price = },
+				);
+		}
+	}
+}
---
+++ Web Inventory/Models/Inventory.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web_Inventory.Models
+{
+	public class Inventory
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		[DisplayName("Product Name")]
+		public string ProductName { get; set; }
+		[Required]
+		public int Quantity { get; set; }
+		[Required]
+		public int Price { get; set; }
+		[Required]
+		public int TotalPrice { get; set; }
+
+
+		public void CalculateTotalPrice()
+		{
+			TotalPrice = Quantity * Price;
+		}
+	}
+
+}
